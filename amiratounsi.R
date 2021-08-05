7ce663ba5513d447228960a89b66981a32494316
AmiraTounsi <88459905+AmiraTounsi@users.noreply.github.com>
2021-08-06 00:13:13 +0100

    amira tounsi R script

---
+++ amiratounsi.R
@@ -0,0 +1,11 @@
+Name.string <- "Amira Tounsi"
+email.string <- "amira.tounsi@insat.u-carthage.tn"
+slackusername.string <- "@AmiraT"
+biostack.string <- "Data science"
+Twitterhandle.string <- "@Tmiran"
+print(Name.string)
+print(email.string)
+print(slackusername.string)
+print(biostack.string)
+print(Twitterhandle.string)
+
