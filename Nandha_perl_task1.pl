a89fc00def93e0b31f831268219925f3dedb1184
Mennatullah97 <88480083+Mennatullah97@users.noreply.github.com>
2021-08-07 01:29:29 +0200

    Update Nandha_perl_task1.pl

--- Nandha_perl_task1.pl
+++ Nandha_perl_task1.pl
@@ -3,7 +3,8 @@
 $name ="KJ Nandha Kishore";
 $email = "nandhakishorekj97@gmail.com";
 $slack_usrname = "@ Nandha";
+$biostack = "Genomics";
 
-print "\n*************************************************\n";
-print("\tName            :  $name\n\tE-mail          :  $email\n\tSlack user name :  $slack_usrname");
-print "\n*************************************************\n";
+
+print( $name, $email , $slack_usrname , $biostack )
+
